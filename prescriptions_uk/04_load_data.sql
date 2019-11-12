0e9bdb0602e85d6a9822bd947096652c26043ca1
Lennart Nieratschker <56670912+LennartAtExasol@users.noreply.github.com>
2019-11-12 19:59:34 +0100

    Updated code to be able to work with other files
    because of the new JSON parser and file structure code had to be redone

--- prescriptions_uk/04_load_data.sql
+++ prescriptions_uk/04_load_data.sql
@@ -1,51 +1,56 @@
 -- refreshing the metadata for the dataset to check for new files
 -- for delta-loads re-create the staging tbl and then re-load the data
 CREATE OR REPLACE TABLE PRESCRIPTIONS_UK_STAGE.stage_raw_data_urls AS
-SELECT  description, TO_NUMBER(TO_CHAR(TO_DATE('August 2018','MONTH YYYY'), 'YYYYMM'), '999999') period, url
+SELECT  description, TO_NUMBER(TO_CHAR(TO_DATE(period, 'MONTH YYYY'), 'YYYYMM'), '999999') period, site_url, file_name
 FROM   (SELECT PRESCRIPTIONS_UK_STAGE.json_parsing_datapackage()) a
 ORDER BY period;
 
 MERGE
 INTO    PRESCRIPTIONS_UK_STAGE.raw_data_urls tgt
 USING   PRESCRIPTIONS_UK_STAGE.stage_raw_data_urls src
-ON tgt.url = src.url
+ON tgt.file_name = src.file_name
 WHEN NOT MATCHED THEN
 INSERT
 	(
 		description,
 		period,
-		url
+		site_url,
+		file_name
 	)
 	VALUES
 	(
 		description,
 		period,
-		url
-	) ;
+		site_url,
+		file_name
+	);
 	
 ----seems to be an error in the source data
 --UPDATE PRESCRIPTIONS_UK_STAGE.raw_data_urls
 --SET     FILE_NAME = 'T201009CHEM%20SUBS.CSV'
 --WHERE   PERIOD = '201009'
 --AND     DESCRIPTION = 'Chemical names and BNF codes';
+
 --UPDATE PRESCRIPTIONS_UK_STAGE.raw_data_urls
 --SET     FILE_NAME = 'T201103ADDR%20BNFT.CSV'
 --WHERE   PERIOD = '201103'
 --AND     DESCRIPTION = 'Practice codes, names and addresses';
 
-----take a look at the new files to load
---SELECT  *
---FROM    PRESCRIPTIONS_UK_STAGE.raw_data_urls
---WHERE   loaded_timestamp IS NULL;
-----TIP: all data will be loaded, that has no loaded_timestamp.
-----     If you want to load the data on a small machine, you can simply set a dummy loaded_timestamp
----- for older data
-----e.g.:
----- update PRESCRIPTIONS_UK_STAGE.raw_data_urls set loaded_timestamp='0001-01-01 00:00:00' where period <=
----- 201702;
-----this script loads all new data (delta load)
---EXECUTE SCRIPT PRESCRIPTIONS_UK_STAGE.PRESCRIPTIONS_LOAD();
---SELECT  *
---FROM    PRESCRIPTIONS_UK_STAGE.JOB_DETAILS
---ORDER BY run_id DESC,
---	detail_id DESC;
+--take a look at the new files to load
+SELECT  *
+FROM    PRESCRIPTIONS_UK_STAGE.raw_data_urls
+WHERE   loaded_timestamp IS NULL;
+
+
+--TIP: all data will be loaded, that has no loaded_timestamp.
+--     If you want to load the data on a small machine, you can simply set a dummy loaded_timestamp
+--      for older data
+--      e.g.:
+-- update PRESCRIPTIONS_UK_STAGE.raw_data_urls set loaded_timestamp='0001-01-01 00:00:00' where period < 201810;
+
+--this script loads all new data (delta load)
+EXECUTE SCRIPT PRESCRIPTIONS_UK_STAGE.PRESCRIPTIONS_LOAD();
+SELECT  *
+FROM    PRESCRIPTIONS_UK_STAGE.JOB_DETAILS
+ORDER BY run_id DESC,
+	detail_id DESC;
