ed3bd3c6290de0b1bc9810a177873cf8e5dad084
Lennart Nieratschker <56670912+LennartAtExasol@users.noreply.github.com>
2020-05-14 09:31:41 +0200

    Finish updating to new website base

--- prescriptions_uk/03_create_scripts.sql
+++ prescriptions_uk/03_create_scripts.sql
@@ -3,76 +3,33 @@
 CREATE OR REPLACE PYTHON3 SCALAR SCRIPT "PRESCRIPTIONS_UK_STAGE"."JSON_PARSING_DATAPACKAGE" () 
 EMITS ("DESCRIPTION" VARCHAR(100) UTF8,"PERIOD" VARCHAR(20) UTF8, "SITE_URL" VARCHAR(100) UTF8, "FILE_NAME" VARCHAR(30) UTF8) AS
 import requests
-import json
+from lxml import html
+import urllib.parse as parse
 import re
-import time
-
-counters = [3]
-json_dict = {}
-
-
-def check_retry_count(counter):
-    if counters[0] > 0:
-        counter[0] -= 1
-        return True
-    else:
-        raise requests.exceptions.RequestException
-
 
 def run(ctx):
     url = r"https://data.gov.uk/dataset/prescribing-by-gp-practice-presentation-level"
-    expression = r'{"@.+]}'
+    xpath = "//a[@data-ga-event='download']/@href"
 
-    while True:
-        try:
-            site_content = requests.get(url).text
-            filtered = re.findall(expression, site_content)[0]
-            json_dict = json.loads(filtered)
-            break
-        except (json.JSONDecodeError, re.error) as errj:
-            ctx.emit("Site JSON is malformed, trying again.\n" + str(errj), "Null", "Null")
-            if check_retry_count(counters):
-                time.sleep(3)
-                continue
-        except requests.exceptions.HTTPError as errh:
-            ctx.emit("HTTP Error occurred, trying again: \n" + str(errh), "Null", "Null")
-            if check_retry_count(counters):
-                time.sleep(3)
-                continue
-        except requests.exceptions.ConnectionError as errc:
-            ctx.emit("Site could not be reached, trying again: \n" + str(errc), "Null", "Null")
-            if check_retry_count(counters):
-                time.sleep(3)
-                continue
-        except requests.exceptions.Timeout as errt:
-            ctx.emit("Request timed out, trying again: \n" + str(errt), "Null", "Null")
-            if check_retry_count(counters):
-                time.sleep(3)
-                continue
-        except requests.exceptions.RequestException as err:
-            ctx.emit("Undefined exception occurred that is NOT\n"
-                  "-JSON error\n"
-                  "-HTTP error\n"
-                  "-Connection error\n"
-                  "-Timeout\n"
-                  "OR maximum amount of retries was reached.\n", "Null", "Null")
+    dom = requests.get(url).content
+    tree = html.fromstring(dom)
+    results = tree.xpath(xpath)
 
-    for e in json_dict.get("distribution"):
-        if e.get("fileFormat") in ("CSV/ZIP", "ZIP"):
-            continue
-        description = e.get("name").lower()
-        period = re.findall(r"\w+ \w+", e.get("name"))[0]
-        temp_url = e.get("contentUrl")
-        url = re.findall(r".*[\/]", temp_url)[0]
-        file_name = re.findall(r"(?:.(?!\/))+$", temp_url)[0][1:]
-        ctx.emit(description, period, url, file_name)
+    for i in results:
+        if i[-3:].lower() == 'csv':
+            txt = i
+            url_path = re.findall('http.+(?=\/)', txt, re.IGNORECASE)[0] + '/'
+            period = re.findall('(?<=t)\d{6}', txt, re.IGNORECASE)[0]
+            description = parse.unquote(re.findall('(?<=(?<=t)\d{6}).+(?=\.csv)', txt, re.IGNORECASE)[0])
+            file_name = re.findall('(?<=\/)[^\/]+\.csv', txt, re.IGNORECASE)[0]
+            ctx.emit(description, period, url_path, file_name)
 /
 
 --load script (executing this again will only load new data if metadata has been refreshed)
 CREATE OR REPLACE LUA SCRIPT PRESCRIPTIONS_UK_STAGE.PRESCRIPTIONS_LOAD RETURNS TABLE AS
 import('ETL.QUERY_WRAPPER','QW')
 
-function process_file(PERIOD,SITE_URL,FILE_NAME)
+function process_file(PERIOD, SITE_URL, FILE_NAME)
         wrapper:set_param('SITE_URL',SITE_URL)
 	wrapper:set_param('FILE_NAME',FILE_NAME)
 	wrapper:set_param('PERIOD',PERIOD)
@@ -99,11 +56,12 @@ function process_file(PERIOD,SITE_URL,FILE_NAME)
 	       REJECT LIMIT :ACCEPTED_ERRORS_PER_FILE]])
 	if (res.etl_rows_with_error > 0) then
 		wrapper:log('WARN','FILE for PERIOD='..wrapper:get_param('PERIOD')..' found rows with errors',res.etl_rows_with_error)
-	end	
+	end
 end
 
+
 --initialize query wrapper
-wrapper = QW.new( 'PRESCRIPTIONS_UK_STAGE.JOB_LOG', 'PRESCRIPTIONS_UK_STAGE.JOB_DETAILS', 'PRESCRIPTIONS_LOAD')
+wrapper = QW.new('PRESCRIPTIONS_UK_STAGE.JOB_LOG', 'PRESCRIPTIONS_UK_STAGE.JOB_DETAILS', 'PRESCRIPTIONS_LOAD')
 
 --set SCHEMA / TABLE for staging table
 wrapper:set_param('STAGE_SCM',quote('PRESCRIPTIONS_UK_STAGE')) 
@@ -113,26 +71,25 @@ wrapper:set_param('PROD_SCM',quote('PRESCRIPTIONS_UK'))
 wrapper:set_param('STAGE_TBL',quote('CHEMICAL_SUBSTANCES'))
 wrapper:set_param('PROD_TBL',quote('CHEMICAL_SUBSTANCES'))
 wrapper:set_param('STAGE_ERROR_TBL',quote('ERRORS_SUBSTANCES'))
-wrapper:set_param('GRP_DESCRIPTION','%chemical%')
+wrapper:set_param('DESCRIPTION','%CHEM SUBS%')
 wrapper:set_param('COL_SELECTION', '(1,2)')
 wrapper:set_param('COL_DESTINATION', '("CHEM_SUB", "CHEMICAL_NAME")')
 wrapper:set_param('SKIP_ROWS',1)
-wrapper:set_param('ROW_SEPARATOR', 'LF')
+wrapper:set_param('ROW_SEPARATOR', 'CRLF')
 wrapper:set_param('ACCEPTED_ERRORS_PER_FILE',10)
 
-for PERIOD,SITE_URL,FILE_NAME in wrapper:query_values( [[
-        SELECT PERIOD,SITE_URL,FILE_NAME 
-        FROM ::STAGE_SCM.RAW_DATA_URLS 
-        WHERE LOADED_TIMESTAMP IS NULL 
-        AND DESCRIPTION LIKE :GRP_DESCRIPTION
-        ORDER BY PERIOD]]) do
+for PERIOD, SITE_URL, FILE_NAME in wrapper:query_values([[      SELECT PERIOD, SITE_URL, FILE_NAME 
+                                                                FROM ::STAGE_SCM.RAW_DATA_URLS 
+                                                                WHERE LOADED_TIMESTAMP IS NULL 
+                                                                AND DESCRIPTION LIKE :DESCRIPTION
+                                                                ORDER BY PERIOD]]) do
         wrapper:set_param('SITE_URL',SITE_URL)
 	wrapper:set_param('PERIOD',PERIOD)
 	wrapper:set_param('FILE_NAME',FILE_NAME)
 
         -- import csv into staging table
-	process_file(PERIOD,SITE_URL,FILE_NAME)
-			
+	process_file(PERIOD, SITE_URL, FILE_NAME)
+		
 	--preparation finished -> move from STAGE into PROD table
 	wrapper:query([[
                 MERGE INTO ::PROD_SCM.::PROD_TBL tgt 
@@ -149,26 +106,25 @@ for PERIOD,SITE_URL,FILE_NAME in wrapper:query_values( [[
                 UPDATE::STAGE_SCM.RAW_DATA_URLS 
                 SET LOADED_TIMESTAMP=CURRENT_TIMESTAMP 
 		WHERE PERIOD=:PERIOD 
-		AND DESCRIPTION LIKE :GRP_DESCRIPTION]])
+		AND DESCRIPTION LIKE :DESCRIPTION]])
 end
 
 -- ****** ADDRESS *******
 wrapper:set_param('STAGE_TBL',quote('PRACTICE_ADDRESS'))
 wrapper:set_param('PROD_TBL',quote('PRACTICE_ADDRESS'))
 wrapper:set_param('STAGE_ERROR_TBL',quote('ERRORS_PRACTICAL_ADDRESS'))
-wrapper:set_param('GRP_DESCRIPTION','%addresses%')
+wrapper:set_param('DESCRIPTION','%ADDR BNFT%')
 wrapper:set_param('COL_SELECTION','(1..8)')
 wrapper:set_param('COL_DESTINATION', ' ')
 wrapper:set_param('SKIP_ROWS',0)
-wrapper:set_param('ROW_SEPARATOR', 'LF')
+wrapper:set_param('ROW_SEPARATOR', 'CRLF')
 wrapper:set_param('ACCEPTED_ERRORS_PER_FILE',10)
 
-for PERIOD,SITE_URL,FILE_NAME in wrapper:query_values( [[
-        SELECT PERIOD,SITE_URL,FILE_NAME 
-        FROM ::STAGE_SCM.RAW_DATA_URLS 
-        WHERE LOADED_TIMESTAMP is NULL 
-        AND DESCRIPTION LIKE:GRP_DESCRIPTION
-        ORDER BY PERIOD]]) do
+for PERIOD,SITE_URL,FILE_NAME in wrapper:query_values([[        SELECT PERIOD, SITE_URL, FILE_NAME 
+                                                                FROM ::STAGE_SCM.RAW_DATA_URLS 
+                                                                WHERE LOADED_TIMESTAMP is NULL 
+                                                                AND DESCRIPTION LIKE:DESCRIPTION
+                                                                ORDER BY PERIOD]]) do
 	process_file(PERIOD,SITE_URL,FILE_NAME) 
         
         --clean up corrupt data before loading into PROD
@@ -203,7 +159,7 @@ for PERIOD,SITE_URL,FILE_NAME in wrapper:query_values( [[
 	wrapper:query([[
                 UPDATE::STAGE_SCM.RAW_DATA_URLS 
                 SET LOADED_TIMESTAMP=CURRENT_TIMESTAMP 
-		WHERE PERIOD=:PERIOD AND DESCRIPTION LIKE:GRP_DESCRIPTION]])
+		WHERE PERIOD=:PERIOD AND DESCRIPTION LIKE:DESCRIPTION]])
 
 end
 
@@ -212,21 +168,21 @@ end
 wrapper:set_param('STAGE_TBL',quote('PRESCRIPTIONS'))
 wrapper:set_param('STAGE_ERROR_TBL',quote('ERRORS_PRESCIPTIONS'))
 wrapper:set_param('PROD_TBL',quote('PRESCRIPTIONS'))
-wrapper:set_param('GRP_DESCRIPTION','%prescribing%')
+wrapper:set_param('DESCRIPTION','%PDPI BNFT%')
 wrapper:set_param('COL_SELECTION','(1..10)')
 wrapper:set_param('SKIP_ROWS',1)
 wrapper:set_param('ROW_SEPARATOR', 'LF')
 wrapper:set_param('ACCEPTED_ERRORS_PER_FILE',10000)
 
-for PERIOD,SITE_URL,FILE_NAME in wrapper:query_values([[
-        SELECT PERIOD,SITE_URL,FILE_NAME FROM ::STAGE_SCM.RAW_DATA_URLS 
-        WHERE LOADED_TIMESTAMP is NULL 
-        AND DESCRIPTION LIKE :GRP_DESCRIPTION
-        ORDER BY PERIOD]]) do
+for PERIOD,SITE_URL,FILE_NAME in wrapper:query_values([[SELECT PERIOD,SITE_URL,FILE_NAME 
+                                                        FROM ::STAGE_SCM.RAW_DATA_URLS 
+                                                        WHERE LOADED_TIMESTAMP is NULL 
+                                                        AND DESCRIPTION LIKE :DESCRIPTION
+                                                        ORDER BY PERIOD]]) do
 	process_file(PERIOD,SITE_URL,FILE_NAME) 
 	
 	--preparation finished -> move from STAGE into PROD table
-	-- edit: left join in first join statement produced errors in prod table because of missing SK_PRACTICE_ADDRESS
+	-- edit: left joins for ::prod_SCM and PROD_SCM.Chemical produce errors because of missing SK_PRACTICE_ADDRESS in ::PROD_SCM.::PROD_TBL 
 	wrapper:query([[
                 INSERT INTO ::PROD_SCM.::PROD_TBL 
                 SELECT  p.*, 
@@ -245,8 +201,7 @@ for PERIOD,SITE_URL,FILE_NAME in wrapper:query_values([[
 	wrapper:query([[
 	       UPDATE ::STAGE_SCM.RAW_DATA_URLS 
 	       SET LOADED_TIMESTAMP=CURRENT_TIMESTAMP
-	       WHERE PERIOD=:PERIOD AND DESCRIPTION LIKE :GRP_DESCRIPTION]])
+--	       WHERE PERIOD=:PERIOD AND DESCRIPTION LIKE :DESCRIPTION]])
 end
---TODO: setup process to re-check if we now have data for *NO_DATA* values
 return wrapper:finish()
 /
--- prescriptions_uk/04_load_data.sql
+++ prescriptions_uk/04_load_data.sql
@@ -1,7 +1,7 @@
 -- refreshing the metadata for the dataset to check for new files
 -- for delta-loads re-create the staging tbl and then re-load the data
 CREATE OR REPLACE TABLE PRESCRIPTIONS_UK_STAGE.stage_raw_data_urls AS
-SELECT  description, TO_NUMBER(TO_CHAR(TO_DATE(period, 'MONTH YYYY'), 'YYYYMM'), '999999') period, site_url, file_name
+SELECT  description, TO_NUMBER(period, '999999') period, site_url, file_name
 FROM   (SELECT PRESCRIPTIONS_UK_STAGE.json_parsing_datapackage()) a
 ORDER BY period;
 
@@ -28,27 +28,14 @@ INSERT
 -- cleanup dead URL 
 DELETE FROM PRESCRIPTIONS_UK_STAGE.raw_data_urls WHERE SITE_URL LIKE '%datagov.ic.%';
 
-----seems to be an error in the source data
---UPDATE PRESCRIPTIONS_UK_STAGE.raw_data_urls
---SET     FILE_NAME = 'T201009CHEM%20SUBS.CSV'
---WHERE   PERIOD = '201009'
---AND     DESCRIPTION = 'Chemical names and BNF codes';
-
---UPDATE PRESCRIPTIONS_UK_STAGE.raw_data_urls
---SET     FILE_NAME = 'T201103ADDR%20BNFT.CSV'
---WHERE   PERIOD = '201103'
---AND     DESCRIPTION = 'Practice codes, names and addresses';
-
 --take a look at the new files to load
 SELECT  *
 FROM    PRESCRIPTIONS_UK_STAGE.raw_data_urls
 WHERE   loaded_timestamp IS NULL;
 
-
 --TIP: all data will be loaded, that has no loaded_timestamp.
 --     If you want to load the data on a small machine, you can simply set a dummy loaded_timestamp
---      for older data
---      e.g.:
+--     for older data e.g.:
 -- update PRESCRIPTIONS_UK_STAGE.raw_data_urls set loaded_timestamp='0001-01-01 00:00:00' where period < 201810;
 
 --this script loads all new data (delta load)
