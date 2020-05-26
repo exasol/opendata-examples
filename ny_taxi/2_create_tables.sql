b468bf17d439f8b53262723258fc0f56ea5ac63c
Lennart Nieratschker <56670912+LennartAtExasol@users.noreply.github.com>
2020-05-26 11:54:41 +0200

    Update repo

---
+++ ny_taxi/2_create_tables.sql
@@ -0,0 +1,116 @@
+--setup logging
+CREATE OR REPLACE TABLE
+	NYC_TAXI_STAGE.job_log(
+		run_id INT IDENTITY,
+		script_name VARCHAR(100),
+		status VARCHAR(100),
+		start_time TIMESTAMP DEFAULT SYSTIMESTAMP,
+		end_time TIMESTAMP
+	);
+		
+CREATE OR REPLACE TABLE
+	NYC_TAXI_STAGE.job_details(
+		detail_id INT IDENTITY,
+		run_id INT,
+		log_time TIMESTAMP,
+		log_level VARCHAR(10),
+		log_message VARCHAR(2000),
+		rowcount INT
+	);
+
+--create staging tables for URLs
+CREATE OR REPLACE TABLE NYC_TAXI_STAGE.stage_raw_data_urls(url VARCHAR(1000));
+CREATE OR REPLACE TABLE NYC_TAXI_STAGE.raw_data_urls(   id DECIMAL IDENTITY, 
+                                                        type VARCHAR(50), 
+                                                        trip_month date, 
+                                                        site_url VARCHAR(1000),
+                                                        filename VARCHAR(1000), 
+                                                        loaded_timestamp TIMESTAMP);
+--create staging table for geo clustering
+CREATE OR REPLACE TABLE NYC_TAXI_STAGE.trip_location_id_map (      trip_id decimal(12), 
+                                                                PICKUP_LOCATIONID DECIMAL(3), 
+                                                                DROPFF_LOCATIONID DECIMAL(3));
+
+--stage and prod trip tables
+CREATE OR REPLACE TABLE NYC_TAXI_STAGE.TRIPDATA  (
+        ID                      DECIMAL(12),
+        FILE_ID		        DECIMAL(9),
+        VENDOR_TYPE             VARCHAR(20),
+        VENDOR_ID               VARCHAR(20),
+        PICKUP_DATETIME         TIMESTAMP,
+        DROPOFF_DATETIME        TIMESTAMP,
+        PASSENGER_COUNT         DECIMAL(10,0),
+        TRIP_DISTANCE           DECIMAL(18,3),
+        PICKUP_LONGITUDE        DOUBLE PRECISION,
+        PICKUP_LATITUDE         DOUBLE PRECISION,
+        PICKUP_LOCATIONID       SMALLINT,
+        RATE_CODE_ID            DECIMAL(9,0),
+        STORE_AND_FWD_FLAG      VARCHAR(10) ASCII,
+        DROPOFF_LONGITUDE       DOUBLE PRECISION,
+        DROPOFF_LATITUDE        DOUBLE PRECISION,
+        DROPOFF_LOCATIONID      SMALLINT,
+        PAYMENT_TYPE            varchar(20),
+        FARE_AMOUNT             DECIMAL(9,2),
+        EXTRA                   DECIMAL(9,2),
+        MTA_TAX                 DECIMAL(9,2),
+        TIP_AMOUNT              DECIMAL(9,2),
+        TOLLS_AMOUNT            DECIMAL(9,2),
+        EHAIL_FEE               DECIMAL(9,2),
+        IMPROVEMENT_SURCHARGE   DECIMAL(9,2),
+        TOTAL_AMOUNT            DECIMAL(9,2),
+        TRIP_TYPE		DECIMAL(9),
+        PICK_GEOM               GEOMETRY(4326),
+        DROP_GEOM               GEOMETRY(4326)
+);
+
+CREATE OR REPLACE TABLE NYC_TAXI.TRIPS (
+        ID		      DECIMAL(12),
+        SRC_FILE_ID	      DECIMAL(9),
+        CAB_TYPE_ID           DECIMAL(9),
+        VENDORID              VARCHAR(9) ASCII,
+        PICKUP_DATE           DATE,
+        DROPOFF_DATE          DATE,
+        PICKUP_DATETIME       TIMESTAMP,
+        DROPOFF_DATETIME      TIMESTAMP,
+        STORE_AND_FWD_FLAG    CHAR(1) ASCII,
+        RATE_CODE_ID          DECIMAL(3),
+        PICKUP_LONGITUDE      DOUBLE,
+        PICKUP_LATITUDE       DOUBLE,
+        PICKUP_LOCATIONID     DECIMAL(3),
+        DROPOFF_LONGITUDE     DOUBLE,
+        DROPOFF_LATITUDE      DOUBLE,
+        DROPOFF_LOCATIONID    DECIMAL(3),
+        PASSENGER_COUNT       DECIMAL(2),
+        TRIP_DISTANCE         DECIMAL(12,3),
+        FARE_AMOUNT           DECIMAL(9,2),
+        EXTRA                 DECIMAL(9,2),
+        MTA_TAX               DECIMAL(9,2),
+        TIP_AMOUNT            DECIMAL(9,2),
+        TOLLS_AMOUNT          DECIMAL(9,2),
+        EHAIL_FEE             DECIMAL(9,2),
+        IMPROVEMENT_SURCHARGE DECIMAL(9,2),
+        TOTAL_AMOUNT          DECIMAL(9,2),
+        PAYMENT_TYPE          VARCHAR(20) ASCII,
+        TRIP_TYPE             VARCHAR(10) ASCII);
+
+-- LOOKUP TABLES
+CREATE OR REPLACE TABLE NYC_TAXI.cab_types (    cab_type_id     DECIMAL(1) IDENTITY PRIMARY KEY,
+                                                type            VARCHAR(20) ASCII);
+
+CREATE OR REPLACE TABLE NYC_TAXI.taxi_zones(    location_id     DECIMAL(3), 
+                                                borough         VARCHAR(20), 
+                                                zone_name       VARCHAR(50), 
+                                                service_zone    VARCHAR(20),
+                                                zone_length     DOUBLE,
+                                                polygon         GEOMETRY(4326),
+                                                zone_area       DOUBLE);
+                        
+CREATE OR REPLACE TABLE NYC_TAXI.vendor_lookup( vendor_id       SMALLINT,
+                                                vendor_name     VARCHAR(200),
+                                                vendor_short    VARCHAR(200));
+
+CREATE OR REPLACE TABLE NYC_TAXI.trip_type_lookup(      trip_type_id    DECIMAL(1),
+                                                        trip_type       VARCHAR(200));
+                
+CREATE OR REPLACE TABLE NYC_TAXI.rate_code_lookup(      rate_code_id    DECIMAL(1),
+                                                        rate_code       VARCHAR(100));
---
+++ ny_taxi/4_import_data.sql
@@ -0,0 +1,95 @@
+--import zones
+CREATE OR REPLACE TABLE NYC_TAXI_STAGE.TMP_TAXI_ZONES (
+        LOCATION_ID SMALLINT, 
+        BOROUGH VARCHAR(15), 
+        "ZONE" VARCHAR(50), 
+        SERVICE_ZONE VARCHAR(20));   
+
+IMPORT INTO NYC_TAXI_STAGE."TMP_TAXI_ZONES" 
+FROM CSV AT 'https://s3.amazonaws.com/nyc-tlc/misc/'
+FILE 'taxi+_zone_lookup.csv'
+SKIP = 1
+ROW SEPARATOR = 'CRLF'
+COLUMN DELIMITER = '"';
+        
+CREATE OR REPLACE TABLE NYC_TAXI_STAGE.TMP_TAXI_ZONE_POLYGONS (
+        LOCATION_ID SMALLINT, 
+        ZONE_LENGTH DOUBLE,
+        POLYGON VARCHAR(500000),
+        ZONE_AREA DOUBLE,
+        ZONE_NAME VARCHAR(200),
+        BOROUGH VARCHAR(200));
+
+--import polygon strings
+IMPORT INTO NYC_TAXI_STAGE."TMP_TAXI_ZONE_POLYGONS" 
+FROM LOCAL CSV FILE 'C:\Users\ln\Documents\GitHub\opendata-examples\ny_taxi\taxi_zones(2).csv' (1..5, 7)
+SKIP = 1
+ROW SEPARATOR = 'LF'
+COLUMN SEPARATOR = ';'
+COLUMN DELIMITER = '"';
+
+--join polygons and zones into PROD
+INSERT INTO NYC_TAXI.TAXI_ZONES
+select z.LOCATION_ID, z.BOROUGH, p.ZONE_NAME, z.SERVICE_ZONE, p.ZONE_LENGTH, p.POLYGON, p.ZONE_AREA
+from NYC_TAXI_STAGE.TMP_TAXI_ZONES z
+join NYC_TAXI_STAGE.TMP_TAXI_ZONE_POLYGONS p on z.location_id = p.location_id;
+
+drop table NYC_TAXI_STAGE."TMP_TAXI_ZONE_POLYGONS" ;
+drop table NYC_TAXI_STAGE."TMP_TAXI_ZONES" ;
+
+-- cleanup
+truncate table NYC_TAXI_STAGE.STAGE_RAW_DATA_URLS;
+--import URLs
+import into NYC_TAXI_STAGE.STAGE_RAW_DATA_URLS
+from csv at 'https://raw.githubusercontent.com/toddwschneider/nyc-taxi-data/master/setup_files/' file 'raw_data_urls.txt' row separator = 'LF';
+
+--manipulate URLS
+merge into NYC_TAXI_STAGE.RAW_DATA_URLS tgt 
+using (
+	with vv_filename as (  select 
+	                       replace(substr(url,instr(url,'/',-1)+1),'.csv','') as filename,url 
+	                       from    NYC_TAXI_STAGE.STAGE_RAW_DATA_URLS),
+	                               vv_splitted as (select  substr(filename,0,instr(filename,'_')-1) as type,
+	                                                       to_date(substr(filename,instr(filename,'tripdata_') + length('tripdate_')),'YYYY-MM') as trip_month,
+	                                                       substr(url,0,instr(url,'/',-1)-1) as site_url,
+	                                                       substr(url,instr(url,'/',-1)+1) as full_file 
+	                                               from vv_filename)
+	select * from vv_splitted) src on tgt.type=src.type and tgt.trip_month=src.trip_month
+when not matched then insert (type,trip_month,site_url,filename) values (type,trip_month,site_url,full_file);
+
+--fill cab_types
+INSERT INTO NYC_TAXI.cab_types (type) SELECT 'yellow';
+INSERT INTO NYC_TAXI.cab_types (type) SELECT 'green';
+INSERT INTO NYC_TAXI.cab_types (type) SELECT 'uber';
+
+--fill vendor_lookup
+INSERT INTO NYC_TAXI.vendor_lookup VALUES (1, 'Creative Mobile Technologies', 'CMT');
+INSERT INTO NYC_TAXI.vendor_lookup VALUES (2, 'VeriFone Inc.', 'VTS');
+INSERT INTO NYC_TAXI.vendor_lookup VALUES (3, 'Digital Dispatch Systems', 'DDS');
+
+--fill trip_type_lookup
+INSERT INTO NYC_TAXI.trip_type_lookup VALUES (1, 'Street-hail');
+INSERT INTO NYC_TAXI.trip_type_lookup VALUES (2, 'Dispatch');
+
+--fill rate_code_lookup
+INSERT INTO NYC_TAXI.rate_code_lookup VALUES (1, 'Standard rate');
+INSERT INTO NYC_TAXI.rate_code_lookup VALUES (2, 'JFK');
+INSERT INTO NYC_TAXI.rate_code_lookup VALUES (3, 'Newark');
+INSERT INTO NYC_TAXI.rate_code_lookup VALUES (4, 'Nassau or Westchester');
+INSERT INTO NYC_TAXI.rate_code_lookup VALUES (5, 'Negotiated fare');
+INSERT INTO NYC_TAXI.rate_code_lookup VALUES (6, 'Group ride');
+
+-- load data
+--TIP: all data will be loaded, that has no loaded_timestamp.
+--     If you want to load the data on a small machine, you can simply set a dummy loaded_timestamp
+--     for older data e.g.:
+-- update NYC_TAXI_STAGE.RAW_DATA_URLS set loaded_timestamp='0001-01-01 00:00:00' where trip_month > '2009-12-01';
+
+--update NYC_TAXI_STAGE.RAW_DATA_URLS set loaded_timestamp='0001-01-01 00:00:00' where trip_month > '2009-12-01';
+--update NYC_TAXI_STAGE.RAW_DATA_URLS set loaded_timestamp=Null where true;
+
+EXECUTE SCRIPT NYC_TAXI_STAGE.TRIPS_LOAD();
+
+select * from NYC_TAXI_STAGE.JOB_DETAILS order by run_id desc,detail_id desc;
+select * from NYC_TAXI_STAGE.ERRORS_TRIPDATA;
+
